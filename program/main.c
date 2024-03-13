b72b0405077b433b323e639012cf29b2ad57b8dc
Hugo Marty <hugo.marty@etu.hesge.ch>
2024-03-13 14:31:16 +0100

    Refactorisation du code

--- program/main.c
+++ program/main.c
@@ -20,6 +20,7 @@
 
 #define COOKIE_SIZE 250
 #define PRICE_MULTIPLIER 1.15
+int marging_top = 0;
 
 struct modifier
 {
@@ -29,6 +30,9 @@ struct modifier
     long double ops;
 };
 
+/// @brief Prépare le texte à écrire
+/// @param result pointeur vers le buffer de sortie
+/// @param value valeur à afficher
 void prepare_to_write(char *result, long double value)
 {
     double value_double = value;
@@ -68,28 +72,97 @@ void prepare_to_write(char *result, long double value)
     }
 }
 
-int main()
+/// @brief Affiche les modificateurs
+/// @param level le niveau du joueur
+/// @param count le nombre de cookies
+/// @param multiplier_modifier le modificateur de clic
+/// @param modifiers les modificateurs
+/// @param buffer un buffer pour stocker les valeurs
+/// @param buffer_bis un second buffer pour stocker les valeurs
+void displayModifiers(int level, long double count, struct modifier* multiplier_modifier, struct modifier* modifiers, char* buffer[100], char* buffer_bis[100])
+{
+    // Background
+    gfx_color(215, 218, 222);
+    gfx_fillrect(SCREEN_WIDTH / 2 - 20, 20  - marging_top, SCREEN_WIDTH / 2 + 750, 100 - marging_top);
+    gfx_fillrect(SCREEN_WIDTH / 2 - 20, 120  - marging_top, SCREEN_WIDTH / 2 + 750, 200 - marging_top);
+    for (int i = 1; i <= level; i++)
+    {
+        gfx_fillrect(SCREEN_WIDTH / 2 - 20, 120 + i * 100  - marging_top, SCREEN_WIDTH / 2 + 750, 200 + i * 100 - marging_top);
+    }
+    gfx_color(237, 241, 245);
+    gfx_fillrect(SCREEN_WIDTH / 2 - 20, 220 + level * 100  - marging_top, SCREEN_WIDTH / 2 + 750, 300 + level * 100 - marging_top);
+    
+    // Click multiplicator
+    if (count >= multiplier_modifier->cost)
+    {
+        gfx_color(0, 0, 0);
+    }
+    else
+    {
+        gfx_color(94, 96, 98);
+    }
+
+    strcpy(buffer_bis, "click multiplier");
+    prepare_to_write(buffer, multiplier_modifier->level);
+    strcat(buffer_bis, buffer);
+    gfx_drawPixelString(SCREEN_WIDTH / 2, 50 - marging_top, buffer_bis, 5);
+
+    strcpy(buffer_bis, "cost");
+    prepare_to_write(buffer, multiplier_modifier->cost);
+    strcat(buffer_bis, buffer);
+    gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 35 - marging_top, buffer_bis, 3);
+
+    strcpy(buffer_bis, "cpc");
+    prepare_to_write(buffer, multiplier_modifier->level);
+    strcat(buffer_bis, buffer);
+    gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 70 - marging_top, buffer_bis, 3);
+    
+    // Modifiers
+    for (int i = 0; i < level + 1; i++)
+    {
+        if (count >= modifiers[i].cost)
+        {
+            gfx_color(0, 0, 0);
+        }
+        else
+        {
+            gfx_color(94, 96, 98);
+        }
+        strcpy(buffer_bis, modifiers[i].name);
+        prepare_to_write(buffer, modifiers[i].level);
+        strcat(buffer_bis, buffer);
+        gfx_drawPixelString(SCREEN_WIDTH / 2, 150 + i * 100  - marging_top, buffer_bis, 5);
+
+        strcpy(buffer_bis, "cost");
+        prepare_to_write(buffer, modifiers[i].cost);
+        strcat(buffer_bis, buffer);
+        gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 135 + i * 100 - marging_top, buffer_bis, 3);
+
+        strcpy(buffer_bis, "cps");
+        prepare_to_write(buffer, modifiers[i].ops);
+        strcat(buffer_bis, buffer);
+        gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 170 + i * 100 - marging_top, buffer_bis, 3);
+    }
+    if (level < 15)
+    {
+        gfx_color(94, 96, 98);
+        gfx_drawPixelString(SCREEN_WIDTH / 2 + 260, 250 + level * 100  - marging_top, "locked ?", 5);
+    }
+}
+
+/// @brief Initialise la librairie graphique
+void initGFX()
 {
     srand(time(NULL));
     gfx_open(SCREEN_WIDTH, SCREEN_HEIGHT, "Ore Clicker");
     gfx_clear_color(255, 255, 255);
     gfx_clear();
+}
 
-    ///
-    long double count = 0;
-    int level = 0;
-    int multiplier = 1;
-    int tick = 0;
-    int animation_tick = 0;
-    int cookie_size = COOKIE_SIZE;
-    int marging_top = 0;
-    ///
-    long double cookies_per_second = 0;
-    ///
-    int click_multiplier = 1;
-    long double click_multiplier_cost = 15;
-    /// Modifiers
-    struct modifier* modifiers = malloc(NUMBER_OF_MODIFIERS * sizeof(struct modifier));
+/// @brief Initialise les modificateurs
+/// @param modifiers pointeur vers le tableau des modificateurs
+void initModifiers(struct modifier* modifiers)
+{
     // Apprentice
     modifiers[0].level = 0;
     modifiers[0].cost = 100;
@@ -175,13 +248,19 @@ int main()
     modifiers[16].cost = 120000000000000000000;
     modifiers[16].ops = 8300000000000;
     strcpy(modifiers[16].name, "github copilot");
+}
 
-    //* Initialize
-    // Read the save file
+/// @brief Lit le fichier de sauvegarde
+/// @param level le niveau du joueur
+/// @param count le nombre de cookies
+/// @param multiplier_modifier le modificateur de clic
+/// @param modifiers les modificateurs
+void readSaveFile(int* level, long double* count, struct modifier* multiplier_modifier, struct modifier* modifiers)
+{
     FILE *file = fopen("save.txt", "rw");
     if (file != NULL)
     {
-        int result = fscanf(file, "%d %Le %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &level, &count, &click_multiplier, &modifiers[0].level, &modifiers[1].level, &modifiers[2].level, &modifiers[3].level, &modifiers[4].level, &modifiers[5].level, &modifiers[6].level, &modifiers[7].level, &modifiers[8].level, &modifiers[9].level, &modifiers[10].level, &modifiers[11].level, &modifiers[12].level, &modifiers[13].level, &modifiers[14].level, &modifiers[15].level, &modifiers[16].level);
+        int result = fscanf(file, "%d %Le %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &level, &count, &multiplier_modifier->level, &modifiers[0].level, &modifiers[1].level, &modifiers[2].level, &modifiers[3].level, &modifiers[4].level, &modifiers[5].level, &modifiers[6].level, &modifiers[7].level, &modifiers[8].level, &modifiers[9].level, &modifiers[10].level, &modifiers[11].level, &modifiers[12].level, &modifiers[13].level, &modifiers[14].level, &modifiers[15].level, &modifiers[16].level);
         if (result == 0)
         {
             printf("Error while reading the save file\n");
@@ -189,14 +268,122 @@ int main()
         fclose(file);
     }
     ///
-    click_multiplier_cost *= pow(PRICE_MULTIPLIER, click_multiplier);
+    multiplier_modifier->level *= pow(PRICE_MULTIPLIER, multiplier_modifier->level);
     for (int i = 0; i < NUMBER_OF_MODIFIERS; i++)
     {
         modifiers[i].cost *= pow(PRICE_MULTIPLIER, modifiers[i].level);
     }
+}
+
+/// @brief Écrit l'état actuel du jeu dans un fichier
+/// @param level le niveau du joueur
+/// @param count le nombre de cookies
+/// @param multiplier_modifier le modificateur de clic
+/// @param modifiers les modificateurs
+void writeSaveFile(int level, long double count, struct modifier* multiplier_modifier, struct modifier* modifiers)
+{
+    FILE* file = fopen("save.txt", "w");
+    if (file != NULL)
+    {
+        int result = fprintf(file, "%d %Le %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &level, &count, &multiplier_modifier->level, &modifiers[0].level, &modifiers[1].level, &modifiers[2].level, &modifiers[3].level, &modifiers[4].level, &modifiers[5].level, &modifiers[6].level, &modifiers[7].level, &modifiers[8].level, &modifiers[9].level, &modifiers[10].level, &modifiers[11].level, &modifiers[12].level, &modifiers[13].level, &modifiers[14].level, &modifiers[15].level, &modifiers[16].level);
+        if (result == 0)
+        {
+            printf("Error while writing the save file\n");
+        }
+    }
+    fclose(file);
+}
+
+int calculatePlayerLevel(int level, long double count)
+{
+    if (count >= 550 && level == 0)
+    {
+        return 1;
+    }
+    if (count >= 6000 && level == 1)
+    {
+        return 2;
+    }
+    if (count >= 65000 && level == 2)
+    {
+        return 3;
+    }
+    if (count >= 700000 && level == 3)
+    {
+        return 4;
+    }
+    if (count >= 10000000 && level == 4)
+    {
+        return 5;
+    }
+    if (count >= 165000000 && level == 5)
+    {
+        return 6;
+    }
+    if (count >= 2550000000 && level == 6)
+    {
+        return 7;
+    }
+    if (count >= 37500000000 && level == 7)
+    {
+        return 8;
+    }
+    if (count >= 500000000000 && level == 8)
+    {
+        return 9;
+    }
+    if (count >= 7000000000000 && level == 9)
+    {
+        return 10;
+    }
+    if (count >= 85000000000000 && level == 10)
+    {
+        return 11;
+    }
+    if (count >= 1050000000000000 && level == 11)
+    {
+        return 12;
+    }
+    if (count >= 13000000000000000 && level == 12)
+    {
+        return 13;
+    }
+    if (count >= 155000000000000000 && level == 13)
+    {
+        return 14;
+    }
+    if (count >= 35500000000000000000 && level == 14)
+    {
+        return 15;
+    }
+    return 0;
+}
+
+
+int main()
+{
+    long double count = 0;
+    int level = 0;
+    int multiplier = 1;
+    int tick = 0;
+    int animation_tick = 0;
+    int cookie_size = COOKIE_SIZE;
+    ///
+    long double cookies_per_second = 0;
+    /// Modificateurs
+    struct modifier multiplier_modifier;
+    struct modifier* modifiers = malloc(NUMBER_OF_MODIFIERS * sizeof(struct modifier));
     ///
 
-    //* Main loop
+    //* Initialisation
+    initGFX();
+    initModifiers(modifiers);
+    multiplier_modifier.level = 1;
+    multiplier_modifier.cost = 15;
+    // Lecture de la sauvegarde
+    readSaveFile(&level, &count, &multiplier_modifier, modifiers);
+
+    //* Game loop
     while (1)
     {
         tick += 1;
@@ -206,7 +393,7 @@ int main()
         {
             if (input == '\e' || input == 'Q')
                 break;
-            // Mouse scroll
+            // Scroll
             if (input == 4)
             {
                 if (marging_top > 0)
@@ -219,29 +406,29 @@ int main()
                 marging_top += 25;
             }
             if (input == 1)
-            { // Left-Mouse-Button
+            { // Bouton gauche de la souris
                 int x = gfx_xNow();
                 int y = gfx_yNow();
                 if (gfx_get_pixel_at(x, y) == MAKE_COLOR(242, 204, 157) || gfx_get_pixel_at(x, y) == MAKE_COLOR(113, 56, 18))
                 {
-                    multiplier = 1 + click_multiplier / 25;
-                    count += 1 * click_multiplier * multiplier;
+                    multiplier = 1 + multiplier_modifier.level / 25;
+                    count += 1 * multiplier_modifier.level * multiplier;
                     animation_tick = tick;
                     cookie_size = cookie_size - 10;
                 }
                 if (x >= SCREEN_WIDTH / 2 - 20 && x <= SCREEN_WIDTH - 140)
                 {
-                    // Click multiplier
+                    // Multiplicateur de clic
                     if (y >= 20 - marging_top && y <= 100 - marging_top)
                     {
-                        if (count >= click_multiplier_cost)
+                        if (count >= multiplier_modifier.cost)
                         {
-                            click_multiplier += 1;
-                            count -= click_multiplier_cost;
-                            click_multiplier_cost *= PRICE_MULTIPLIER;
+                            multiplier_modifier.level += 1;
+                            count -= multiplier_modifier.cost;
+                            multiplier_modifier.cost *= PRICE_MULTIPLIER;
                         }
                     }
-                    /// Modifiers
+                    /// Modificateurs
                     for (int i = 0; i < NUMBER_OF_MODIFIERS; i++)
                     {
                         if (y >= 120 + i * 100 - marging_top && y <= 200 + i * 100 - marging_top)
@@ -258,81 +445,23 @@ int main()
             }
         }
 
-        //* Process
+        //* Processus
+        // Calcule le nombre de cookies par seconde
         cookies_per_second = 0;
         for (int i = 0; i < NUMBER_OF_MODIFIERS; i++)
         {
             cookies_per_second += modifiers[i].ops * modifiers[i].level;
         }
-        // Player levels
-        if (count >= 550 && level == 0)
-        {
-            level = 1;
-        }
-        if (count >= 6000 && level == 1)
-        {
-            level = 2;
-        }
-        if (count >= 65000 && level == 2)
-        {
-            level = 3;
-        }
-        if (count >= 700000 && level == 3)
-        {
-            level = 4;
-        }
-        if (count >= 10000000 && level == 4)
-        {
-            level = 5;
-        }
-        if (count >= 165000000 && level == 5)
-        {
-            level = 6;
-        }
-        if (count >= 2550000000 && level == 6)
-        {
-            level = 7;
-        }
-        if (count >= 37500000000 && level == 7)
-        {
-            level = 8;
-        }
-        if (count >= 500000000000 && level == 8)
-        {
-            level = 9;
-        }
-        if (count >= 7000000000000 && level == 9)
-        {
-            level = 10;
-        }
-        if (count >= 85000000000000 && level == 10)
-        {
-            level = 11;
-        }
-        if (count >= 1050000000000000 && level == 11)
-        {
-            level = 12;
-        }
-        if (count >= 13000000000000000 && level == 12)
-        {
-            level = 13;
-        }
-        if (count >= 155000000000000000 && level == 13)
-        {
-            level = 14;
-        }
-        if (count >= 35500000000000000000 && level == 14)
-        {
-            level = 15;
-        }
-    
-        // Ores per second/10
+        // Augmente le nombre de cookies toutes les 10 centièmes de secondes
         if (tick % 100 == 0)
         {
             count += cookies_per_second/10;
         }
 
-        // Planet size
+        // Calcule le niveau du joueur
+        level = calculatePlayerLevel(level,count);
+
+        // Reset la taille du cookie
         if ((tick - animation_tick) >= 25)
         {
             cookie_size = COOKIE_SIZE;
@@ -343,92 +472,27 @@ int main()
         char buffer[100];
         char buffer_bis[100];
         //* Interface
-        // The cookie
+        // Dessine le cookie
         gfx_full_circle(SCREEN_WIDTH / 4 - 20, SCREEN_HEIGHT / 2, cookie_size, MAKE_COLOR(242, 204, 157));
-        // Draw the chocolate chips
+        // et les pépites de chocolat
         gfx_full_circle(SCREEN_WIDTH / 4 - 20 - cookie_size / 2, SCREEN_HEIGHT / 2 - cookie_size / 2.5, cookie_size / 5, MAKE_COLOR(113, 56, 18));
         gfx_full_circle(SCREEN_WIDTH / 4 - 20, SCREEN_HEIGHT / 2 - 20, cookie_size / 10, MAKE_COLOR(113, 56, 18));
         gfx_full_circle(SCREEN_WIDTH / 4 - 20 + cookie_size / 5, SCREEN_HEIGHT / 2 + cookie_size / 2, cookie_size / 5, MAKE_COLOR(113, 56, 18));
         gfx_full_circle(SCREEN_WIDTH / 4 - 20 - cookie_size / 2.5, SCREEN_HEIGHT / 2 + cookie_size / 3.33, cookie_size / 6.66, MAKE_COLOR(113, 56, 18));
         gfx_full_circle(SCREEN_WIDTH / 4 - 20 + cookie_size / 2, SCREEN_HEIGHT / 2 - cookie_size / 2.5, cookie_size / 6.66, MAKE_COLOR(113, 56, 18));
 
-        // The modifiers
-        gfx_color(215, 218, 222);
-        gfx_fillrect(SCREEN_WIDTH / 2 - 20, 20  - marging_top, SCREEN_WIDTH / 2 + 750, 100 - marging_top);
-        gfx_fillrect(SCREEN_WIDTH / 2 - 20, 120  - marging_top, SCREEN_WIDTH / 2 + 750, 200 - marging_top);
-        for (int i = 1; i <= level; i++)
-        {
-            gfx_fillrect(SCREEN_WIDTH / 2 - 20, 120 + i * 100  - marging_top, SCREEN_WIDTH / 2 + 750, 200 + i * 100 - marging_top);
-        }
-        gfx_color(237, 241, 245);
-        gfx_fillrect(SCREEN_WIDTH / 2 - 20, 220 + level * 100  - marging_top, SCREEN_WIDTH / 2 + 750, 300 + level * 100 - marging_top);
-
+        // Les modificateurs
         gfx_color(0, 0, 0);
-        // Click multiplicator
-        if (count >= click_multiplier_cost)
-        {
-            gfx_color(0, 0, 0);
-        }
-        else
-        {
-            gfx_color(94, 96, 98);
-        }
-
-        strcpy(buffer_bis, "click multiplier");
-        prepare_to_write(buffer, click_multiplier);
-        strcat(buffer_bis, buffer);
-        gfx_drawPixelString(SCREEN_WIDTH / 2, 50 - marging_top, buffer_bis, 5);
-
-        strcpy(buffer_bis, "cost");
-        prepare_to_write(buffer, click_multiplier_cost);
-        strcat(buffer_bis, buffer);
-        gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 35 - marging_top, buffer_bis, 3);
-
-        strcpy(buffer_bis, "cpc");
-        prepare_to_write(buffer, click_multiplier);
-        strcat(buffer_bis, buffer);
-        gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 70 - marging_top, buffer_bis, 3);
-
-        /// Modifiers
-        for (int i = 0; i < level + 1; i++)
-        {
-            if (count >= modifiers[i].cost)
-            {
-                gfx_color(0, 0, 0);
-            }
-            else
-            {
-                gfx_color(94, 96, 98);
-            }
-            strcpy(buffer_bis, modifiers[i].name);
-            prepare_to_write(buffer, modifiers[i].level);
-            strcat(buffer_bis, buffer);
-            gfx_drawPixelString(SCREEN_WIDTH / 2, 150 + i * 100  - marging_top, buffer_bis, 5);
-
-            strcpy(buffer_bis, "cost");
-            prepare_to_write(buffer, modifiers[i].cost);
-            strcat(buffer_bis, buffer);
-            gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 135 + i * 100 - marging_top, buffer_bis, 3);
-
-            strcpy(buffer_bis, "cps");
-            prepare_to_write(buffer, modifiers[i].ops);
-            strcat(buffer_bis, buffer);
-            gfx_drawPixelString(SCREEN_WIDTH/2 + 580, 170 + i * 100 - marging_top, buffer_bis, 3);
-        }
-        if (level < 15)
-        {
-            gfx_color(94, 96, 98);
-            gfx_drawPixelString(SCREEN_WIDTH / 2 + 260, 250 + level * 100  - marging_top, "locked ?", 5);
-        }
+        displayModifiers(level, count, &multiplier_modifier, modifiers, buffer, buffer_bis);
 
         //* Informations
         gfx_color(0, 0, 0);
-        // The count
+        // Le nombre de cookies
         strcpy(buffer_bis, "count");
         prepare_to_write(buffer, count);
         strcat(buffer_bis, buffer);
         gfx_drawPixelString(20, 20, buffer_bis, 7);
-        // The ores per second
+        // Le nombre de cookies par seconde
         strcpy(buffer_bis, "cpc");
         prepare_to_write(buffer, cookies_per_second);
         strcat(buffer_bis, buffer);
@@ -438,16 +502,7 @@ int main()
         usleep(1000);
     }
     gfx_flush();
-    // Save the count and the click multiplier
-    file = fopen("save.txt", "w");
-    if (file != NULL)
-    {
-        int result = fprintf(file, "%d %Le %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", level, count, click_multiplier, modifiers[0].level, modifiers[1].level, modifiers[2].level, modifiers[3].level, modifiers[4].level, modifiers[5].level, modifiers[6].level, modifiers[7].level, modifiers[8].level, modifiers[9].level, modifiers[10].level, modifiers[11].level, modifiers[12].level, modifiers[13].level, modifiers[14].level, modifiers[15].level, modifiers[16].level);
-        if (result == 0)
-        {
-            printf("Error while writing the save file\n");
-        }
-    }
-    fclose(file);
+    // Sauvegarde
+    writeSaveFile(level, count, &multiplier_modifier, modifiers);
     return EXIT_SUCCESS;
 }
